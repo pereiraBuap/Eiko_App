b21091c2b244da18a4656c6b5b71e83daf8e25da
ChristianPastenC <pastencristian2@gmail.com>
2020-06-16 18:59:02 -0500

    Bug Arreglado

--- App/Eiko_App/src/app/home/home.page.ts
+++ App/Eiko_App/src/app/home/home.page.ts
@@ -9,24 +9,45 @@ import { ModalPage } from '../modal/modal.page';
 })
 export class HomePage implements OnInit {
   
+  dark: Boolean;
+  first: Boolean;
+  second: Boolean;
+  third: Boolean;
+
   constructor(public modalCtrl: ModalController) {}
 
   ngOnInit(){  }
 
   darkTheme(){
     document.body.classList.toggle('dark');
+    this.dark = true;
+    this.first = false;
+    this.second = false;
+    this.third = false;
   }
   
   firstTheme(){
     document.body.classList.toggle('first');
+    this.dark = false;
+    this.first = true;
+    this.second = false;
+    this.third = false;
   }
 
   secondTheme(){
     document.body.classList.toggle('second');
+    this.dark = false;
+    this.first = false;
+    this.second = true;
+    this.third = false;
   }
 
   thirdTheme(){
     document.body.classList.toggle('third');
+    this.dark = false;
+    this.first = false;
+    this.second = false;
+    this.third = true;
   }
 
   async presentModal() {
